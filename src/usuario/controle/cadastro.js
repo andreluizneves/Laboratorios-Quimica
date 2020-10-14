a9b7c0a459b561feca629fbff906657be7a4f337
MarioGuilherme <marioguietec@gmail.com>
2020-10-14 14:36:31 -0300

    Ajustes WS Cadastro e pequenas mudanças no Front

--- src/usuario/controle/cadastro.js
+++ src/usuario/controle/cadastro.js
@@ -5,16 +5,22 @@ $(document).ready(function() {
             $('.img').attr('src', '../../../recursos/img/alunos.svg')
             $('#rm').show(490)
             $('#ra').hide()
+            $('#ra').prop('disabled', true)
+            $('#rm').prop('disabled', false)
+            $('#ra').val('')
         } else {
             $('.img').attr('src', '../../../recursos/img/professores.svg')
             $('#rm').hide()
             $('#ra').show(490)
+            $('#ra').prop('disabled', false)
+            $('#rm').prop('disabled', true)
+            $('#rm').val('')
         }
     })
 
     $('.btn-cadastro').click(function(e) {
 
-        e.preventDefault
+        e.preventDefault()
 
         var dados = $('#form-cadastro').serialize();
 
@@ -26,11 +32,13 @@ $(document).ready(function() {
             data: dados,
             success: function(dados) {
                 Swal.fire({
-                    icon: 'success',
-                    html: '<h1 style="color:white;">Cadastrado com êxito</h1>',
+                    icon: dados.icone,
+                    html: '<h2 style="color:white;">' + dados.mensagem + '</h2>',
                     background: 'rgb(39, 39, 61)',
                 })
-                $('.input').val('')
+                if (dados.icone == 'success') {
+                    $('.input').val('')
+                }
             }
         });
     })
--- src/usuario/modelo/cadastro.php
+++ src/usuario/modelo/cadastro.php
@@ -8,33 +8,43 @@
         $tipousuario = $requestData['tipo-usuario'];
         
         if($tipousuario == 'professor(a)'){
-
             $sql = "INSERT INTO professor (nome, email, ra, senha) VALUES ('$requestData[nome]', '$requestData[email]', '$requestData[ra]', '$requestData[senha]')";
-
+            if($requestData['nome']  == '' || $requestData['email'] == '' || $requestData['ra'] == '' || $requestData['senha'] == '' ){
+                $dados = array(
+                    'mensagem' => 'Há campos em vazios que precisam ser preenchidos',
+                    'icone' => 'error'
+                );
+                echo json_encode($dados, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+                exit;
+            }
         } else{
-
             $sql = "INSERT INTO aluno (nome, email, rm, senha) VALUES ('$requestData[nome]', '$requestData[email]', '$requestData[rm]', '$requestData[senha]')";
-
+            if($requestData['nome']  == '' || $requestData['email'] == '' || $requestData['rm'] == '' || $requestData['senha'] == '' ){
+                $dados = array(
+                    'mensagem' => 'Há campos em vazios que precisam ser preenchidos',
+                    'icone' => 'error'
+                );
+                echo json_encode($dados, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+                exit;
+            }
         }
+
 
         $resultado = mysqli_query($conexao, $sql);
 
         if($resultado){
             $dados = array(
                 'mensagem' => "Cadastrado com êxito.",
+                'icone' => 'success'
             );
         } else {
             $dados = array(
-                'mensagem' => "Não foi possível realizar o cadastro."
+                'mensagem' => "Não foi possível realizar o cadastro.",
+                'icone' => 'error'
             );
         }       
         
         mysqli_close($conexao);
-
-    } else {
-        $dados = array(
-            'mensagem' => 'Falha na conexão'
-        );
     }
 
     echo json_encode($dados, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
